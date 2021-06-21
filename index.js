b75f2cdfd1d151e7c79d8bfeb689b4733305c094
Harshadjoshi01 <harshadonly01@gmail.com>
2021-06-21 22:49:31 +0530

    given port

--- index.js
+++ index.js
@@ -576,6 +576,6 @@ app.post("/pinchange", async function(req, res){
 
 })
 
-app.listen(3000, function() {
-  console.log("server is running @ port 3000");
+app.listen( process.env.PORT|| 3000, function() {
+    console.log("server is running @ port");
 });
