a81b0c67e250ca425ea34ee0cf932d293588c635
Harshadjoshi01 <harshadonly01@gmail.com>
2021-06-24 13:13:09 +0530

    solving mysql error

--- index.js
+++ index.js
@@ -41,16 +41,43 @@ var options = {
 };
 
 var sessionStore = new MySQLStore(options);
-const db = mysql.createConnection(options);
-db.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+// const db = mysql.createConnection(options);
+// db.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
  
-  console.log('connected as id ' + db.threadId);
-});
-// app.use(cookieParser('secret'))
+//   console.log('connected as id ' + db.threadId);
+// });
+
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(options); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+
 app.use(
   session({
     secret: "secret",
