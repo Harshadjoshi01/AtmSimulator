438af1469384a8016faec27b49587ceeeee7421e
Harshadjoshi01 <harshadonly01@gmail.com>
2021-06-24 12:26:39 +0530

    changes in port variable

--- index.js
+++ index.js
@@ -627,5 +627,5 @@ app.post("/pinchange", async function(req, res){
 const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-    console.log("server is running @ port");
+    console.log("server is running @ port: "+port.toString());
 });
